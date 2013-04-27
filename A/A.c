12d49c43a696683253b3b01f17957a7749e95bfc
Przemyslaw Sierocinski <przsie@gmail.com>
2013-04-27 19:26:38 +0200

    modified:   A/A.c

--- A/A.c
+++ A/A.c
@@ -1,12 +1,12 @@
-/* Przemyslaw Sierocinski
- * i248066
- */
+// finding shortest path to destination
+// BFS
+
 #include <stdio.h>
 #include <stdlib.h>
 
 #define QSIZE 1500*2000
 
-char graf[2000*2000]; // 4MB
+char graph[2000*2000]; // 4MB
 
 struct element
 	{
@@ -19,7 +19,7 @@ struct element e;
 
 unsigned qbeg, qend, last;
 
-inline void addqueue(int id, int length)
+void addqueue(int id, int length)
 {
 	last = 0;
 	queue[qend].id = id;
@@ -31,14 +31,12 @@ inline void addqueue(int id, int length)
 	return;
 }
 
-inline void delqueue()
+void delqueue()
 {
 	if(last)
 	{
-		printf("NIE");
+		printf("NIE"); // no route to destination
 		exit(0);
-		//e.id = 1<<31;
-		//return e;
 	}
 
 	if(qbeg == qend)
@@ -57,7 +55,7 @@ int main()
 	int m,n,p,nm;
 	register unsigned i,k;
 
-	//memset(graf, 'x', 2000*2000);
+	//memset(graph, 'x', 2000*2000);
 
 	scanf("%u%u", &m, &n);
 
@@ -65,13 +63,13 @@ int main()
 
 	for(i=0,k=0; i<n; ++i)
 	{
-		fread(graf+k, 1, m, stdin);
+		fread(graph+k, 1, m, stdin);
 		fgetc(stdin); // '\n'
 		k+=m;
 	}
 
 	for(k=0; k<m*n; ++k)
-		if(graf[k] == 'P')
+		if(graph[k] == 'P')
 		{
 			p=k;
 			break;
@@ -85,7 +83,7 @@ int main()
 	last = 0;
 
 	addqueue(p, 0);
-	graf[p] = 'x';
+	graph[p] = 'x';
 
 	nm=n*m;
 
@@ -93,54 +91,52 @@ int main()
 	{
 		delqueue();
 		if(e.id+m < nm)
-			if(graf[e.id+m] != 'x')
+			if(graph[e.id+m] != 'x')
 			{
-				if(graf[e.id+m] == 'S')
+				if(graph[e.id+m] == 'S')
 				{
 					printf("%d", e.plength+1);
 					return 0;
 				}
 				addqueue(e.id+m, e.plength+1);
-				graf[e.id+m] = 'x';
+				graph[e.id+m] = 'x';
 			}
 		if(e.id%m != m-1 && e.id+1 < nm)
-			if(graf[e.id+1] != 'x')
+			if(graph[e.id+1] != 'x')
 			{
-				if(graf[e.id+1] == 'S')
+				if(graph[e.id+1] == 'S')
 				{
 					printf("%d", e.plength+1);
 					return 0;
 				}
 				addqueue(e.id+1, e.plength+1);
-				graf[e.id+1] = 'x';
+				graph[e.id+1] = 'x';
 			}
 		if(e.id%m > 0 && e.id-1 >= 0)
-			if(graf[e.id-1] != 'x')
+			if(graph[e.id-1] != 'x')
 			{
-				if(graf[e.id-1] == 'S')
+				if(graph[e.id-1] == 'S')
 				{
 					printf("%d", e.plength+1);
 					return 0;
 				}
 				addqueue(e.id-1, e.plength+1);
-				graf[e.id-1] = 'x';
+				graph[e.id-1] = 'x';
 			}
 		if(e.id-m >= 0)
-			if(graf[e.id-m] != 'x')
+			if(graph[e.id-m] != 'x')
 			{
-				if(graf[e.id-m] == 'S')
+				if(graph[e.id-m] == 'S')
 				{
 					printf("%d", e.plength+1);
 					return 0;
 				}
 				addqueue(e.id-m, e.plength+1);
-				graf[e.id-m] = 'x';
+				graph[e.id-m] = 'x';
 			}
 		//printf("\n");
 		//printgraph(m,n);
 	}
-
-	//printf("NIE");
 
 	return 0;
 }
